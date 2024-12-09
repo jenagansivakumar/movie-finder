101ec119148b5beade8dcf97fec4722cf52fa62f
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-09 18:32:25 +0100

    filtering results

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -14,6 +15,8 @@ var Movies = []Movie{
 	{Title: "test", Genre: "test", Rating: 0},
 	{Title: "test1", Genre: "test1", Rating: 0},
 	{Title: "test2", Genre: "test2", Rating: 0},
+	{Title: "jdka", Genre: "kdjfka", Rating: 939},
+	{Title: "jdka", Genre: "Sci-Fi", Rating: 939},
 }
 
 func handleHealth(w http.ResponseWriter, r *http.Request) {
@@ -21,15 +24,27 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRecommendations(w http.ResponseWriter, r *http.Request) {
+	genre := r.URL.Query().Get("genre")
+	fmt.Println(genre)
 	var movieName string = ""
+
 	for _, movie := range Movies {
-		movieName += movie.Title + "\n"
+		if genre == "" {
+			movieName += movie.Title + "\n"
+		} else {
+			if genre == movie.Genre {
+
+				movieName = movie.Title + "\n"
+			}
+		}
+
+		w.Write([]byte(movieName))
+
 	}
-	w.Write([]byte(movieName))
 }
 
 func main() {
-	http.HandleFunc("/recommendations?genre=Sci-Fi", handleRecommendations)
+	http.HandleFunc("/recommendations", handleRecommendations)
 	http.HandleFunc("/health", handleHealth)
 	http.ListenAndServe(":8080", nil)
 }
