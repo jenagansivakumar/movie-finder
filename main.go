e33c316fd68d0b55d0b852ae8ee9bf88fa309e35
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-09 13:33:44 +0100

    created movies struct and creating movies now

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func healthChecker(w http.ResponseWriter, r *http.Request) {
+type Movie struct {
+	Title  string
+	Genre  string
+	Rating float64
+}
+
+var Moves = []Movie{
+	{Title: "test", Genre: "test", Rating: 0},
+	{Title: "test1", Genre: "test1", Rating: 0},
+	{Title: "test2", Genre: "test2", Rating: 0},
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
+
+func handleRecommendations(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Movie recommendations will go here"))
+}
+
 func main() {
-	http.HandleFunc("/health", healthChecker)
+	http.HandleFunc("/recommendations", handleRecommendations)
+	http.HandleFunc("/health", handleHealth)
+
 	http.ListenAndServe(":8080", nil)
 }
