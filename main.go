e231955fe5a0ce527a4c5da65107c99eea39c792
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-12 19:38:55 +0100

    unmarshalled json

--- main.go
+++ main.go
@@ -3,76 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-var tmdbApiKey string
-
-func getDotEnv(key string) string {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatalf("Error loading .env file", err)
-
-	}
-
-	value := os.Getenv(key)
-	if value == "" {
-		log.Fatal("Environment variable %s is not set or empty", key)
-	}
-
-	return value
+type MovieStruct struct {
+	Title string
+	Genre string
+	Adult string
 }
 
-type Movie struct {
-	Title  string
-	Genre  string
-	Rating float64
-}
-
-var Movies = []Movie{
-	{Title: "Parasite", Genre: "Drama", Rating: 8.6},
-	{Title: "Oldboy", Genre: "Thriller", Rating: 8.4},
-	{Title: "The Handmaiden", Genre: "Romance", Rating: 8.1},
-	{Title: "I Saw the Devil", Genre: "Horror", Rating: 7.8},
-	{Title: "Train to Busan", Genre: "Action", Rating: 7.6},
-	{Title: "Memories of Murder", Genre: "Crime", Rating: 8.1},
-	{Title: "Sympathy for Lady Vengeance", Genre: "Thriller", Rating: 7.6},
-	{Title: "The Wailing", Genre: "Horror", Rating: 7.4},
-	{Title: "A Tale of Two Sisters", Genre: "Horror", Rating: 7.1},
-	{Title: "Burning", Genre: "Mystery", Rating: 7.5},
-}
-
-func getRecommendation(w http.ResponseWriter, r *http.Request) {
-	genre := r.URL.Query().Get("genre")
-	movieList := []string{}
-
-	if genre == "" {
-		w.Write([]byte("Genre has not been speicified"))
-		return
-	}
-
-	for _, movie := range Movies {
-		if genre == movie.Genre {
-			movieList = append(movieList, movie.Title)
-		}
-	}
-	if len(movieList) == 0 {
-		w.Write([]byte("Movielist is empty!"))
-		return
-	}
-
-	jsonData, err := json.Marshal(movieList)
-	if err != nil {
-		http.Error(w, "Error encoding the  data", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonData)
+func getApiKey() string {
+	godotenv.Load()
+	apiKey := os.Getenv("API_KEY")
+	return apiKey
 
 }
 
@@ -80,10 +27,36 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func getRecommendations(w http.ResponseWriter, r *http.Request) {
+	// genre := r.URL.Query().Get("genre")
+	apiKey := getApiKey()
+	url := fmt.Sprintf("https://api.themoviedb.org/3/movie/popular?api_key=%s", apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Error retrieving response from url", http.StatusInternalServerError)
+		return
+	}
+	jsonData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		w.Write([]byte("json data not found"))
+		return
+	}
+	resp.Body.Close()
+	fmt.Println(string(jsonData))
+
+	var movieResponse MovieStruct
+
+	err = json.Unmarshal(jsonData, &movieResponse)
+	if err != nil {
+		http.Error(w, "Error unmarshalling json", http.StatusInternalServerError)
+	}
+
+	fmt.Println(movieResponse)
+
+}
+
 func main() {
-	tmdbApiKey = getDotEnv("TMDB_API_KEY")
-	fmt.Printf(tmdbApiKey)
-	http.HandleFunc("/recommendations", getRecommendation)
+	http.HandleFunc("/recommendations", getRecommendations)
 	http.HandleFunc("/health", healthCheck)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 }
