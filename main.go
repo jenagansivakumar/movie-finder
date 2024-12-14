af36506de57c940d78091dbc6521b5dc8ff5fed5
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-14 20:51:16 +0100

    added redis and it's working

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 )
 
@@ -32,6 +35,15 @@ func init() {
 	}
 }
 
+var redisClient *redis.Client
+
+func initRedis() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	return client
+}
+
 func getApiKey() string {
 	return os.Getenv("API_KEY")
 }
@@ -40,6 +52,21 @@ func getResults(w http.ResponseWriter, r *http.Request) {
 
 	apiKey := getApiKey()
 	url := fmt.Sprintf("https://api.themoviedb.org/3/movie/popular?api_key=%s", apiKey)
+
+	ctx := context.Background()
+
+	cachedData, err := redisClient.Get(ctx, url).Result()
+	if err == redis.Nil {
+		fmt.Println("Cannot find data in cache")
+
+	} else if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(cachedData))
+		return
+	}
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -56,6 +83,19 @@ func getResults(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	encodedJson, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, "Error encoding data for cache", http.StatusInternalServerError)
+		return
+	}
+
+	err = redisClient.Set(ctx, url, encodedJson, 10*time.Minute).Err()
+	if err != nil {
+		log.Printf("Failed to set data in Redis: %v", err)
+	} else {
+		log.Println("Data added to cache")
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(results)
 	if err != nil {
@@ -66,6 +106,9 @@ func getResults(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	redisClient = initRedis()
+
+	fmt.Println(redisClient)
 
 	http.HandleFunc("/", getResults)
 	err := http.ListenAndServe(":8080", nil)
