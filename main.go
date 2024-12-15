1f8a78ee0eca23ba1e7a7b7f12d3c945b1612138
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-15 17:39:15 +0100

    using go routine to concurrently fetch api if cache isn't being used

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ var limiterMap = make(map[string]*rate.Limiter)
 
 func initRedis() {
 	redisClient = redis.NewClient(&redis.Options{
+		// Addr: "redis-cache:6379",
 		Addr: "localhost:6379",
 	})
 	fmt.Println("redis initialised")
@@ -67,35 +68,38 @@ func getResults(w http.ResponseWriter, r *http.Request, redisClient *redis.Clien
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 			return
 		}
-		fmt.Println("Fetching data from API")
-		resp, err := http.Get(url)
-		if err != nil {
-			http.Error(w, "Cannot retrieve response from url", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
 
-		var results TotalResults
-		err = json.NewDecoder(resp.Body).Decode(&results)
-		if err != nil {
-			http.Error(w, "Error decoding json", http.StatusInternalServerError)
-			return
-		}
+		go func() {
+			fmt.Println("Go routine: fetching data from api")
+			resp, err := http.Get(url)
+			if err != nil {
+				http.Error(w, "Cannot retrieve response from url", http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
 
-		jsonData, err := json.Marshal(results)
-		if err != nil {
-			http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
-			return
-		}
+			var results TotalResults
+			err = json.NewDecoder(resp.Body).Decode(&results)
+			if err != nil {
+				http.Error(w, "Error decoding json", http.StatusInternalServerError)
+				return
+			}
 
-		err = redisClient.Set(ctx, url, string(jsonData), 10*time.Minute).Err()
-		if err != nil {
-			http.Error(w, "error adding json to the cache", http.StatusInternalServerError)
-			return
-		}
-		fmt.Println("setting cached data")
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonData)
+			jsonData, err := json.Marshal(results)
+			if err != nil {
+				http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+				return
+			}
+
+			err = redisClient.Set(ctx, url, string(jsonData), 10*time.Minute).Err()
+			if err != nil {
+				http.Error(w, "error adding json to the cache", http.StatusInternalServerError)
+				return
+			}
+			fmt.Println("setting cached data")
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonData)
+		}()
 
 	} else if err != nil {
 		fmt.Printf("Error connecting to redisc: %v\n", err)
